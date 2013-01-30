e096a29157a65a49d509a31a45e8c2a10926f042
Pawel Becela <pawbecela@gmail.com>
2013-01-30 14:52:06 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BecelaStore::Application.config.secret_token = '46846364c01ff1fb8fdfcf1c17ef83a7b93388dd03a0e237efaad43b68e7b6b5c471b993adaeb7b9b372170ab76c9400690182482042ec9c50d5bd249cff3ceb'
